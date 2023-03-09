5cf12a9fe96e81e6377f2e958a1729c96e731f83
ADITHYA THEJAS S <adithyathejass.1997@gmail.com>
2023-03-09 17:52:43 +0530

    error-input-added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 // import axios
 import axios from "axios";
@@ -19,7 +19,7 @@ import {
   BsCloudHaze2Fill,
   BsCloudDrizzleFill,
   BsThermometer,
-  BsWind,
+  // BsWind,
 } from "react-icons/bs";
 
 import { TbTemperatureCelsius } from "react-icons/tb";
@@ -32,7 +32,7 @@ const App = () => {
   const [data, setData] = useState(null);
   const [location, setLocation] = useState("");
   const [inputValue, setInputValue] = useState("");
-  const [animate, setAnimate] = useState(false);
+  // const [animate, setAnimate] = useState(false);
   const [loading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
   const [tab, setTab] = useState("Home");
@@ -41,6 +41,7 @@ const App = () => {
     setInputValue(e.target.value);
     console.log("E Target value : ", e.target.value);
     setLocation(e.target.value);
+    setErrorMsg("");
   };
 
   const handleSubmit = (e) => {
@@ -120,47 +121,55 @@ const App = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(fetchCurrent);
     } else {
-      setErrorMsg("Your browser not support geolocation");
+      setErrorMsg("user denied geolocation");
     }
   }
 
   return (
     <div className="w-full h-screen  bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0 bg-[url('./assets/img/bgupdated.png')]">
       {tab === "Home" && (
-        <div className="p-20 md:w-1/2 rounded overflow-hidden shadow-lg ">
-          <h3 className=" justify-center not-italic font-semibold text-2xl leading-7 flex items-center text-center text-teal-900">
+        <div className="p-20 md:w-1/2 rounded overflow-hidden shadow-lg">
+          <h3 className="mb-16 justify-center not-italic font-semibold text-2xl leading-7 flex items-center text-center text-teal-900">
             Weather App
           </h3>
           <form>
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center flex-col">
               {loading && (
-                <div>
-                  <input placeholder="loading..." />
+                <div className="w-3/4 flex items-center justify-center mb-4">
+                  <input
+                    placeholder="loading..."
+                    className="h-10 w-3/4 px-6 bg-gray-300"
+                  />
                 </div>
               )}
               {errorMsg && (
-                <div>
-                  <input placeholder={errorMsg} />
+                <div className="w-3/4 flex items-center justify-center mb-4">
+                  <input
+                    placeholder={errorMsg}
+                    className="h-10 w-3/4 px-6 bg-pink-200"
+                  />
                 </div>
               )}
-              <input
-                onChange={(e) => handleInput(e)}
-                className="bg-gray-300 w-3/4 p-2 "
-                type="text"
-                placeholder="Enter city name"
-                disabled={loading}
-              />
-              <button
-                onClick={(e) => handleSubmit(e)}
-                disabled={loading}
-                className="bg-teal-900  p-2"
-              >
-                <IoMdSearch className="text-2xl text-white" />
-              </button>
+              <div className="w-3/4 flex items-center justify-center">
+                <input
+                  onChange={(e) => handleInput(e)}
+                  className="h-10 w-96 px-6 bg-gray-300 outline-none"
+                  type="text"
+                  placeholder="Enter city name"
+                  disabled={loading}
+                />
+                <button
+                  onClick={(e) => handleSubmit(e)}
+                  disabled={loading}
+                  className="bg-teal-900  p-2"
+                >
+                  <IoMdSearch className="text-2xl text-white" />
+                </button>
+              </div>
             </div>
             <div class="inline-flex items-center justify-center w-full">
               <hr class="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-              <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">
+              <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-black">
                 or
               </span>
             </div>
@@ -168,7 +177,7 @@ const App = () => {
               <button
                 onClick={(e) => getCurrentLocation(e)}
                 disabled={loading}
-                className="h-10 w-3/4 px-6 text-indigo-100 transition-colors duration-150  focus:shadow-outline  bg-teal-600"
+                className="h-10 w-96 px-6 text-indigo-100 transition-colors duration-150  focus:shadow-outline  bg-teal-600"
               >
                 Use Device Location
               </button>
@@ -177,21 +186,21 @@ const App = () => {
         </div>
       )}
       {tab === "result" && (
-        <div className="w-full max-w-[450px] bg-gray-900 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6">
+        <div className="w-full max-w-[450px] bg-gray-700 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px]">
           <div>
-            <h3 className="justify-center not-italic  text-2xl  flex items-center text-center">
+            <h3 className="justify-center not-italic mt-10 text-2xl  flex items-center text-center">
               Weather App
             </h3>
             {/* card top */}
             <div className="flex items-center gap-x-5">
               <button onClick={() => setTab("Home")}>
                 {" "}
-                <IoMdArrowBack />
+                <IoMdArrowBack className="ml-4" />
               </button>
               {/* icon */}
               <div>
                 {/* country name */}
-                <div className="text-2xl font-semibold justify-center not-italic  flex items-center text-center">
+                <div className="text-2xl font-semibold justify-center not-italic  flex items-center text-center pt-10 mx-10">
                   <GrLocation />
                   {data.name}, {data.sys.country}
                 </div>
@@ -202,34 +211,31 @@ const App = () => {
               <div className="flex justify-center items-center">
                 {/* temp */}
                 <div className="text-[144px] leading-none font-light">
-                  {/* {parseInt(data.main.temp)} */}
                   <div className="text-[87px]">{getIcon()}</div>
-                </div>
-                {/* celsius icon */}
-                <div className="text-4xl">
-                  {/* <div className="text-[87px]">{getIcon()}</div> */}
-                  {/* <TbTemperatureCelsius /> */}
                 </div>
               </div>
               {/* weather description */}
-              <div className="text-center text-lg">
-                {parseInt(data.main.temp)} C
+              <div className="text-center text-xl mt-6">
+                {data.weather[0].description}
               </div>
-              <div className="text-center">{data.weather[0].description}</div>
+            </div>
+            <div className="text-center text-5xl mb-16">
+              {parseInt(data.main.temp)} C
             </div>
             {/* card bottom */}
-            <div className="max-w-[378px] mx-auto flex flex-col gap-y-4">
+            <div className="mx-auto flex flex-col gap-y-4 bg-teal-600 h-[5rem] rounded-b-lg divide-x divide-blue-200">
               <div className="flex justify-between">
-                <div className="flex items-center gap-x-2">
+                <div className="flex items-center gap-x-2 mt-6">
                   {/* icon */}
                   <div className="text-[20px]">
                     <IoIosWater />
                   </div>
                   <div>
-                    Wind <span className="ml-2">{data.wind.speed} m/s</span>
+                    Humidity{" "}
+                    <span className="ml-2">{data.main.humidity} %</span>
                   </div>
                 </div>
-                <div className="flex items-center gap-x-2">
+                <div className="flex items-center gap-x-2 mt-6">
                   {/* icon */}
                   <div className="text-[20px]">
                     <BsThermometer />
